4167470c3e0d4bc8de641fc0fa98826c20d13c77
garretpremo <garretpremo@gmail.com>
2022-07-06 23:41:18 -0400

    remove starter test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
 import { QuestForest } from "./quest-tree";
 import { MissionName } from "./types";
-
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { quests } from "./data";
 
 test("quest forest", () => {
   const questForest = QuestForest.new();
